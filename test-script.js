4e16acbb3c5ddca26010f0c61760675be11d49bb
Nick Kao <kobenkao@gmail.com>
2021-07-10 11:42:12 -0700

    new iteration of test

--- test-script.js
+++ test-script.js
@@ -1,3 +1,8 @@
-const p = document.createElement('p');
-p.textContent = 'Article';
-document.body.appendChild(p);
+const button = document.getElementById('hello-url');
+
+button.addEventListener('click', () => {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Hello World!';
+  document.body.appendChild(h1);
+});
+
